11bf5bab4df903c36e6af2b65b966fa4c940815f
zidongzh <zidongzh@outlook.com>
2022-11-14 10:52:44 +0800

    update

--- src/Work01.java
+++ src/Work01.java
@@ -11,10 +11,10 @@ public class Work01 {
         char[] seq1;
         char[] seq2;
 
-        seq1 = getSequence();
-        seq2 = getSequence();
-//        seq1 = "AACGTACTCAAGTCT".toCharArray();
-//        seq2 = "TCGTACTCTAACGAT".toCharArray();
+//        seq1 = getSequence();
+//        seq2 = getSequence();
+        seq1 = "AACGTACTCAAGTCT".toCharArray();
+        seq2 = "TCGTACTCTAACGAT".toCharArray();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -23,8 +23,8 @@ public class Work01 {
         System.out.print("Choose rule(1/2):");
         int rule = scanner.nextInt();
 
-        System.out.println("Choose algorithm, 1 for Needleman-Wunsch, 2 for Smith-Waterman:");
-        int algorithm = scanner.nextInt();
+//        System.out.println("Choose algorithm, 1 for Needleman-Wunsch, 2 for Smith-Waterman:");
+//        int algorithm = scanner.nextInt();
 
         int match1 = 9;
         int mismatch1 = -6;
@@ -36,6 +36,8 @@ public class Work01 {
 
         int[][] matrix = rule == 2 ? localAlignment(seq1, seq2, match2, mismatch2, gap2) : localAlignment(seq1, seq2, match1, mismatch1, gap1);
 
+
+        // print result
         for (int i = 0; i <= seq1.length + 1; i++) {
             if (i <= 1) {
                 System.out.printf("%4s", "");
@@ -57,6 +59,10 @@ public class Work01 {
             }
             System.out.println();
         }
+
+        System.out.println("Score: "+matrix[seq1.length+1][seq2.length+1]);
+
+        // print best match
 
     }
 
@@ -137,15 +143,39 @@ public class Work01 {
         return matrix;
     }
 
-    public static void backTrack(int[][] matrix) {
+    public static int[] backTrack(int[][] matrix) {
         int rowLen = matrix.length;
         int colLen = matrix[0].length;
 
-        // init a array that record track
+        // init an array that record track
         // 0: top left, 1: top, -1: left
         int[] track = new int[Math.max(rowLen, colLen) * 2];
+        int t = 0;
         int i = rowLen - 1;
         int j = colLen - 1;
+        while (i >= 0 && j >= 0) {
+            int a = matrix[j][i - 1];
+            int b = matrix[j - 1][i - 1];
+            int c = matrix[j - 1][i];
+
+            int max = Math.max(a, Math.max(b, c));
+
+            if (max == a) {
+                track[t] = 1;
+                i -= 1;
+                t++;
+            } else if (max == b) {
+                track[t] = 0;
+                i -= 1;
+                j -= 1;
+                t++;
+            } else {
+                track[t] = -1;
+                j -= 1;
+                t++;
+            }
+        }
+        return track;
     }
 }
 
